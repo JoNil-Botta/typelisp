c1eaacbfc1e5eb9d6e8a3d3ce46257c76dbf22d3
Botta <botta@jonil.dev>
2026-06-05 09:17:52 +0200

    Stdlib process: add async API stubs (#1856)
    Refs #1847.

--- scripts/verify-stdlib.sh
+++ scripts/verify-stdlib.sh
@@ -208,7 +208,7 @@ stdlib/tests/fs_api.tl|42|-|-|-|requires-stage0-symbol:file-read-chunk-status,fi
 stdlib/tests/hash_api.tl|42|-|-
 stdlib/tests/hashmap_api.tl|42|-|-
 stdlib/tests/list_api.tl|42|-|-
-stdlib/tests/process_api.tl|42|-|-
+stdlib/tests/process_api.tl|42|-|-|-|requires-stage0-symbol:tl_process_start,tl_process_wait
 stdlib/tests/process_runtime.tl|42|-|-
 stdlib/tests/queue_api.tl|42|-|-
 stdlib/tests/random_api.tl|42|-|-|-|requires-stage0-symbol:tl_random_system_seed

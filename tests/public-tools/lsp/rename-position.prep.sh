ffb101ee9b18f0ae3d4da8db4681fceb442b0f01
Jonathan Nilsson <lord.jonil@gmail.com>
2026-08-26 00:36:17 +0200

    lsp: add safe semantic workspace rename

---
+++ tests/public-tools/lsp/rename-position.prep.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -eu
+
+mkdir -p "$FIXTURE_TMP/root/surfaces"
+
+cat > "$FIXTURE_TMP/root/surfaces/lib.tl" <<'EOF'
+(module surfaces.lib)
+(define shared-value : i64 9)
+(defstruct Pair (left i64) (right i64))
+EOF

5b06fe942ee711b9ad6bda31debc487127250909
Jonathan Nilsson <lord.jonil@gmail.com>
2026-09-24 06:44:34 +0200

    test: refresh native import golden for bounded short copies

--- tests/golden/selfhost_compiler_driver_import.s
+++ tests/golden/selfhost_compiler_driver_import.s
@@ -688,7 +688,11 @@ main:
     .globl tl_memcpy
     .type tl_memcpy,@function
 tl_memcpy:
+    cmpq $1, %rdx
+    je .Ltl_memcpy_one
     movq %rdx, %rcx
+    cmpq $64, %rcx
+    jbe .Ltl_memcpy_small
     movq %rsi, %rax
     addq %rcx, %rax
     cmpq %rax, %rdi
@@ -709,11 +713,67 @@ tl_memcpy:
     andq $7, %rcx
     rep movsb
     ret
+.Ltl_memcpy_small:
+    cmpq $16, %rcx
+    ja .Ltl_memcpy_above_16
+    cmpq $8, %rcx
+    jae .Ltl_memcpy_small_16
+    cmpq $4, %rcx
+    jae .Ltl_memcpy_small_8
+    cmpq $2, %rcx
+    jae .Ltl_memcpy_small_4
+    testq %rcx, %rcx
+    je .Ltl_memcpy_small_done
+.Ltl_memcpy_one:
+    movzbl (%rsi), %eax
+    movb %al, (%rdi)
+.Ltl_memcpy_small_done:
+    ret
+.Ltl_memcpy_small_4:
+    movzwl (%rsi), %eax
+    movzwl -2(%rsi,%rcx), %edx
+    movw %ax, (%rdi)
+    movw %dx, -2(%rdi,%rcx)
+    ret
+.Ltl_memcpy_small_8:
+    movl (%rsi), %eax
+    movl -4(%rsi,%rcx), %edx
+    movl %eax, (%rdi)
+    movl %edx, -4(%rdi,%rcx)
+    ret
+.Ltl_memcpy_small_16:
+    movq (%rsi), %rax
+    movq -8(%rsi,%rcx), %rdx
+    movq %rax, (%rdi)
+    movq %rdx, -8(%rdi,%rcx)
+    ret
+.Ltl_memcpy_small_32:
+    movups (%rsi), %xmm0
+    movups -16(%rsi,%rcx), %xmm1
+    movups %xmm0, (%rdi)
+    movups %xmm1, -16(%rdi,%rcx)
+    ret
+.Ltl_memcpy_above_16:
+    cmpq $32, %rcx
+    jbe .Ltl_memcpy_small_32
+    movups (%rsi), %xmm0
+    movups 16(%rsi), %xmm1
+    movups -32(%rsi,%rcx), %xmm2
+    movups -16(%rsi,%rcx), %xmm3
+    movups %xmm0, (%rdi)
+    movups %xmm1, 16(%rdi)
+    movups %xmm2, -32(%rdi,%rcx)
+    movups %xmm3, -16(%rdi,%rcx)
+    ret
     .size tl_memcpy, . - tl_memcpy
     .globl tl_memcpy_fresh
     .type tl_memcpy_fresh,@function
 tl_memcpy_fresh:
+    cmpq $1, %rdx
+    je .Ltl_memcpy_one
     movq %rdx, %rcx
+    cmpq $64, %rcx
+    jbe .Ltl_memcpy_small
     jmp .Ltl_memcpy_fwd
     .size tl_memcpy_fresh, . - tl_memcpy_fresh
     .globl tl_array_zero
